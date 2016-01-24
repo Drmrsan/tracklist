e3c72abc16f0bfaa4e2318e61f678bfc9a29fdd1
Drmrsan <drmrsan@gmail.com>
2016-01-24 23:05:37 +0000

    task_list depended:delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	has_many :task_lists
+	has_many :task_lists, :dependent => :delete_all
 	has_many :task_items, :dependent => :delete_all
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
