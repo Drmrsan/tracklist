c43f008e2f8fe1af78a4f9a1b646e2ad066250f5
drmrsan <drmrsan@gmail.com>
2016-01-25 00:36:28 +0100

    no message

--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -1,4 +1,4 @@
 class TaskList < ActiveRecord::Base
-	has_many :task_items, dependent: :nullify
+	has_many :task_items, dependent: :destroy
 	belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-	has_many :task_lists, dependent: :nullify
-	has_many :task_items, dependent: :nullify
+	has_many :task_lists, dependent: :destroy
+	has_many :task_items, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
